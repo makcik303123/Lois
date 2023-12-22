b0a8d4272b9382d6528d4ea547c5734aebf90836
Maksim <adoberoflit@gmail.com>
2023-12-22 18:02:35 +0300

    pravki

--- js/popup.js
+++ js/popup.js
@@ -2,8 +2,6 @@ const img = document.querySelector(".title__rectangles");
 const close = document.querySelector(".popup__wrapper");
 const popup = document.querySelector(".popup-rec");
 
-console.log(popup);
-
 function tooglePopup() {
 	document.body.classList.toggle("lock");
 	popup.classList.toggle("active");
